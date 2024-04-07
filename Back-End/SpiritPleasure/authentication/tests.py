42f0569a7a6a986e35cfb681785830f1516263e6
NotOriginal333 <m.naumovych2@gmail.com>
2024-04-07 17:18:29 +0300

    try_tests

--- Back-End/SpiritPleasure/authentication/tests.py
+++ Back-End/SpiritPleasure/authentication/tests.py
@@ -1,39 +1,26 @@
-# tests.py
+from .serializers import UserRegisterSerializer
+from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-from django.test import TestCase, Client
-from django.urls import reverse
-from .models import CustomUser
-import json
-
-
-class UserIntegrationTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_create_user(self):
-        # Перевірка створення користувача
-        create_user_url = reverse('create_user')
+class UserRegisterSerializerTestCase(TestCase):
+    def test_user_creation(self):
         user_data = {
             'username': 'testuser',
             'email': 'test@example.com',
-            'password': 'testpassword'
+            'password': 'password',
+            'password2': 'password',
         }
-        response = self.client.post(create_user_url, json.dumps(user_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
 
-        # Перевірка, що користувач існує в базі даних
-        self.assertTrue(CustomUser.objects.filter(username='testuser').exists())
+        serializer = UserRegisterSerializer(data=user_data)
+        self.assertTrue(serializer.is_valid())
 
-    def test_get_user(self):
-        # Створюємо тестового користувача
-        user = CustomUser.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        user = serializer.save()
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'test@example.com')
 
-        # Перевірка отримання інформації про користувача
-        get_user_url = reverse('get_user', args=[user.id])
-        response = self.client.get(get_user_url)
-        self.assertEqual(response.status_code, 200)
-
-        # Перевірка, що отримана інформація відповідає даним користувача
-        response_data = response.json()
-        self.assertEqual(response_data['username'], 'testuser')
-        self.assertEqual(response_data['email'], 'test@example.com')
+class CreateAuthTokenTestCase(TestCase):
+    def test_auth_token_creation(self):
+        user = User.objects.create_user(username='testuser', email='test@example.com', password='password')
+        self.assertTrue(Token.objects.filter(user=user).exists())
