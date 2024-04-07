f9b89f50b1a11dd31037f34c787158f589e92354
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2024-04-07 16:10:15 +0300

    fix: modified auth/register

--- Back-End/SpiritPleasure/SpiritPleasure/urls.py
+++ Back-End/SpiritPleasure/SpiritPleasure/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from place.views import PlaceViewSet
-
-
-router = DefaultRouter()
-router.register(r'places', PlaceViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
-    path('api/', include(router.urls)),
+    path('api/', include('authentication.urls'))
 ]
