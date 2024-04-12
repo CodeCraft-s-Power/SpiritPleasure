07dc3c41362e59dc7d9a8b3e7f495875df98c6d0
NotOriginal333 <m.naumovych2@gmail.com>
2024-04-12 23:50:43 +0300

    placeCreate

--- Back-End/SpiritPleasure/place/models.py
+++ Back-End/SpiritPleasure/place/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    postalcode = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.region}, {self.postalcode}"
+
+
 class Place(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='place_images/', blank=True, null=True)
+    location = models.ForeignKey(Address) #, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
